59eb18384c9415a993134032750ebd8074d9aa9c
ljy <ljy@163.com>
2024-01-02 11:22:49 +0800

    [修改] 将选择排序写为函数接口

--- selectSort.c
+++ selectSort.c
@@ -3,12 +3,8 @@
 #define BUFFER_SIZE 6
 
 /* 选择排序：相较于冒泡排序，减少了交换次数 */
-
-int main(int argc, char const *argv[])
+int selectSort(int *array, int arrayLen)
 {
-    int array[BUFFER_SIZE] = {88, 54, 78, 89, 127, 159};
-    int arrayLen = sizeof(array) / sizeof(array[0]);
-
     int minVal = 0;
     int minIndex = 0;
     for(int pos = 0; pos < arrayLen; pos++)
@@ -31,11 +27,26 @@ int main(int argc, char const *argv[])
             array[minIndex] = temp;
         }
     }
+}
 
-    for(int idx = 0; idx < arrayLen; idx++)
+/*数组做函数参数会被弱化为指针 */
+/* 打印函数 */
+int printArray(int *array, int arraySize)
+{
+    for(int idx = 0; idx < arraySize; idx++)
     {
         printf("array[%d] = %d ", idx, array[idx]);
     }
+}
+
+int main(int argc, char const *argv[])
+{
+    int array[BUFFER_SIZE] = {88, 54, 78, 89, 127, 159};
+    int arrayLen = sizeof(array) / sizeof(array[0]);
+
+    selectSort(array, arrayLen);
+
+    printArray(array, arrayLen);
 
     printf("\n");
     return 0;
